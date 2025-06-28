e0ce00fef8971b197f531768c4846b5bb32be84a
Emshs <min5090seo@gmail.com>
2025-06-28 13:38:34 +0900

    ESLInt 무시

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import { build } from "velite";
 /** @type {import('next').NextConfig} */
 export default {
   // othor next config here...
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
     return config;
